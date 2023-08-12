a61be05ef7d300441cf881bf36220090f3acfdc7
Anfe-ion <andres.moncada.c@gmail.com>
2023-08-12 17:36:41 -0500

    Se crea un archivo aparte para el header, ahora se añade con un script hacia header.js

---
+++ header.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.createElement("header");
+    header.innerHTML= `
+    <button class="open-menu" id="openMenu"><i class="bi bi-three-dots"></i></button>
+        <nav class="nav" id="nav">
+            <button class="close-menu" id="closeMenu"><i class="bi bi-x"></i></button>
+            <ul class="nav-list">
+                <li class="search-box">                    
+                    <input type="text" id="searchBar" class="search-bar" placeholder="Busca tu e-bike...">
+                    <button id="search-button" class="search-button"><i class="bi bi-search"></i></button>                    
+                </li>
+                <li><a href="#">Todas las e-bikes</a></li>
+                <li><a href="#">Accesorios</a></li>
+                <li><a href="#">¿Por qué elegir una e-bike?</a></li>
+                <li><a href="#">Historias</a></li>
+                <li><a href="#">Quienes somos</a></li>
+                <li><a href="#">Soporte</a></li>
+                <div class="login-section">
+                    <li><a href="#">Entrar</a></li>
+                    <li><a href="#">Registrarse</a></li>
+                    <li><a href="#">¿Olvidaste tu contraseña?</a></li>
+                </div>
+                <div class="social-media">
+                    <button><a href="#"><i class="bi bi-facebook"></i></a></button>
+                    <button><a href="#"><i class="bi bi-instagram"></i></a></button>
+                    <button><a href="#"><i class="bi bi-twitter"></i></a></button>
+                    <button><a href="#"><i class="bi bi-whatsapp"></i></a></button>
+                </div>
+            </ul>
+        </nav>
+
+        <h1><a href="./index.html">yellowCat</a></h1>
+
+        <div class="buttons">
+            <button class="search" id="search"><i class="bi bi-search"></i></button>
+            <button class="shop-car"><i class="bi bi-cart"></i></button>
+            <button class="login"><i class="bi bi-person"></i></button>
+        </div>
+    `
+    document.body.insertBefore(header, document.body.firstChild);
+});
--- menu.js
+++ menu.js
@@ -1,18 +1,21 @@
-const nav = document.querySelector("#nav");
-const openMenu = document.querySelector("#openMenu");
-const search = document.querySelector("#search");
-const closeMenu = document.querySelector("#closeMenu");
+//It adds DOMContentLoaded because without it the menu doesn't work
+document.addEventListener("DOMContentLoaded", () => {
+    const nav = document.querySelector("#nav");
+    const openMenu = document.querySelector("#openMenu");
+    const search = document.querySelector("#search");
+    const closeMenu = document.querySelector("#closeMenu");
 
-//Fuction for open menu
-function showNav(){
-    nav.classList.add("visible");
-}
+    //Function for open menu
+    function showNav(){
+        nav.classList.add("visible");
+    }
 
-//Adding the open action to search button and menu button
-openMenu.addEventListener("click", showNav);
-search.addEventListener("click", showNav);
+    //Adding the open action to search button and menu button
+    openMenu.addEventListener("click", showNav);
+    search.addEventListener("click", showNav);
 
-//Close the menu with class removing
-closeMenu.addEventListener("click", ()=> {
-    nav.classList.remove("visible");
+    //Close the menu with class removing
+    closeMenu.addEventListener("click", ()=> {
+        nav.classList.remove("visible");
+    })
 })

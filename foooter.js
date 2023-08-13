33c0c85ee5afbda5727cd4d981cd5d2c075d8f94
Anfe-ion <andres.moncada.c@gmail.com>
2023-08-13 11:23:01 -0500

    Se realiza otra section y se añade el footer, todo responsive y con particular-desing para una mejor eficiencia.

---
+++ foooter.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const footer = document.createElement("footer");
+    footer.innerHTML= `
+    <h6>yellowCat</h6>
+        <a href="">Garantía</a>
+        <a href="">Registro</a>
+        <a href="">Financiamiento</a>
+        <a href="">Programa de afiliados</a>
+        <div>
+            <button><a href="#"><i class="bi bi-facebook"></i></a></button>
+            <button><a href="#"><i class="bi bi-instagram"></i></a></button>
+            <button><a href="#"><i class="bi bi-twitter"></i></a></button>
+            <button><a href="#"><i class="bi bi-whatsapp"></i></a></button>
+        </div>
+    `
+    document.body.appendChild(footer, document.body.firstChild);
+});
